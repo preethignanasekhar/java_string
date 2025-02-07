ba7aa520a2bebcd255886a15582e5d9b2106b267
Preethi Gnanasekhar <preethignanasekhar444@gmail.com>
2025-02-07 21:32:53 +0530

    Add files via upload

---
+++ reversed.java
@@ -0,0 +1,14 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="hello";
+	    String reversed="";
+	    for(int i=str.length()-1;i>=0;i--){
+	        reversed+=str.charAt(i);
+	    
+	    }
+	      System.out.println(reversed);
+		
+	}
+}
+
