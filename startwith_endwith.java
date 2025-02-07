3db4957b16c8da0fd9dea42057065ed83924762b
Preethi Gnanasekhar <preethignanasekhar444@gmail.com>
2025-02-07 21:31:32 +0530

    Add files via upload

---
+++ startwith_endwith.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str1="hello world";
+
+		System.out.println(str1.startsWith("hello"));
+		System.out.println(str1.endsWith("hello"));
+	}
+}
+
