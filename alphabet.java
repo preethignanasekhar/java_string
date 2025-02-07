d2ba32cba688d180dce01ee6cb31f78fc61fa1d1
Preethi Gnanasekhar <preethignanasekhar444@gmail.com>
2025-02-07 21:34:29 +0530

    Add files via upload

---
+++ alphabet.java
@@ -0,0 +1,14 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="pree1thi";
+	   if(str.matches(".[a-z A-Z]+.")){
+	       System.out.println("true");
+	   }
+	   else{
+	       System.out.println("false");
+	   }
+	   
+	}
+}
---
+++ index_of.java
@@ -0,0 +1,7 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str1="hello world";
+	    System.out.println(str1.indexOf('e'));
+	}
+}
