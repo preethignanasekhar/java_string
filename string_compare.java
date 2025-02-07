9c3264a508915f5c32d10cf989e514214dc929fd
Preethi Gnanasekhar <preethignanasekhar444@gmail.com>
2025-02-07 20:53:09 +0530

    Add files via upload

---
+++ string_compare.java
@@ -0,0 +1,10 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    String str1="hello world";
+	    String str2="HELLO WORLD";
+	   
+		System.out.println(str1.equals(str2) );
+	}
+}
---
+++ string_length.java
@@ -0,0 +1,9 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    String str1="hello world";
+	    int len=str1.length();
+		System.out.println("print "+len);
+	}
+}
